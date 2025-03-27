fea3e0be3086df9356f608ac966c634a5676ec99
brummer10 <brummer-@web.de>
2025-03-27 19:15:32 +0100

    Switch to use RubberBandStretcher::OptionEngineFiner

--- alooper/vs.c
+++ alooper/vs.c
@@ -28,15 +28,17 @@ void free_desinterleaved_buffer(const float *const *buffer, int channel_count) {
 Varispeed::Varispeed() {
     rubberband_input_buffers = allocate_desinterleaved_buffer(MAX_RUBBERBAND_CHANNELS, MAX_RUBBERBAND_BUFFER_FRAMES);
     rubberband_output_buffers = allocate_desinterleaved_buffer(MAX_RUBBERBAND_CHANNELS, MAX_RUBBERBAND_BUFFER_FRAMES);
-
 }
 Varispeed::~Varispeed() {
     free_desinterleaved_buffer(rubberband_input_buffers, MAX_RUBBERBAND_CHANNELS);
     free_desinterleaved_buffer(rubberband_output_buffers, MAX_RUBBERBAND_CHANNELS);
 }
 void Varispeed::initialize(uint32_t sr) {
-    RubberBand::RubberBandStretcher::Options rb_options = RubberBand::RubberBandStretcher::OptionProcessRealTime;
+    RubberBand::RubberBandStretcher::Options rb_options = RubberBand::RubberBandStretcher::OptionProcessRealTime |
+        RubberBand::RubberBandStretcher::OptionEngineFiner;
     int stereo_channel_count = 2;
     rb = std::make_unique<RubberBand::RubberBandStretcher>(sr, stereo_channel_count, rb_options);
     rb->setMaxProcessSize(MAX_RUBBERBAND_BUFFER_FRAMES);
+    rb->process( rubberband_input_buffers,MAX_RUBBERBAND_BUFFER_FRAMES,false);
+    rb->reset();
 }
